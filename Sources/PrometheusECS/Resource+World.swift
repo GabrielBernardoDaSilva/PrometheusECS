ab1ad46225e9e5646de940259dd2fdd5f505dc56
GabrielBernardoDaSilva <gabriel7798@gmail.com>
2025-01-03 19:57:10 -0300

    Feat: Add resources interface through world

---
+++ Sources/PrometheusECS/Resource+World.swift
@@ -0,0 +1,21 @@
+//
+//  Resource+World.swift
+//  PrometheusECS
+//
+//  Created by Gabriel Bernardo on 03/01/25.
+//
+
+extension World {
+    public func addResource<R: Resource>(_ resource: R) {
+        resourceManager.createResource(resource)
+    }
+    
+    @discardableResult
+    func removeResource<R: ResourceProtocol>() -> R? {
+        return resourceManager.removeResource()
+    }
+    
+    public func getResource<R: ResourceProtocol>(_ type: R.Type) -> R? {
+        return resourceManager.getResource(type)
+    }
+}
